97afe36a18bd5aa22461f310fa6733c6b2d2b840
GamebP <88152063+GamebP@users.noreply.github.com>
2024-03-12 09:31:53 +0200

    Update ekransavins.pas

--- Algoritmu Iztrāde/ekransavins.pas
+++ Algoritmu Iztrāde/ekransavins.pas
@@ -1,4 +1,4 @@
-Program RandomSymbols;
+Program Ekransavins;
 
 Uses
 Crt;
@@ -17,29 +17,29 @@ Begin
 Starts:
   Randomize;
   Clrscr;
-  maxX := 100; // Iegûst loga platumu
-  maxY := 26;  // Iegûst loga augstumu
+  maxX := 100; // IegÃ»st loga platumu
+  maxY := 26;  // IegÃ»st loga augstumu
   count := 0;
 
 	TextColor(White);
-  Write('Spied <Esc>, lai aizvçrtu, vai citu taustiòu, lai notîrîtu logu...');
+  Write('Spied <Esc>, lai aizvÃ§rtu, vai citu taustiÃ²u, lai notÃ®rÃ®tu logu...');
 
 	Repeat
     Gotoxy(Random(maxX) + 1, Random(maxY) + 1);
-    // Pârvieto kursoru uz random X un Y pozîciju
+    // PÃ¢rvieto kursoru uz random X un Y pozÃ®ciju
     If Wherey = 1 Then
       Begin
         If Wherex <= Neizmantot Then
           continue;
-        // Pâriet pie nâkamâ cikla soli, ja simbols ir aizliegts
+        // PÃ¢riet pie nÃ¢kamÃ¢ cikla soli, ja simbols ir aizliegts
       End;
 			
-    TextColor(Random(15) + 1); // Izvçlçto krâsu
-    Write(Chr(Random(254 - 33) + 33)); // Random simbols no ASCII 33 lîdz 254
+    TextColor(Random(15) + 1); // IzvÃ§lÃ§to krÃ¢su
+    Write(Chr(Random(254 - 33) + 33)); // Random simbols no ASCII 33 lÃ®dz 254
     Delay(300); // Aizkave 300 milisekundes (0.3 sekundes)
     Inc(count);
 		
-  Until (Keypressed); // Iziet no cikla, ja nospiests ESC vai ir sasniegts maksimâlais simbolu skaits
+  Until (Keypressed); // Iziet no cikla, ja nospiests ESC vai ir sasniegts maksimÃ¢lais simbolu skaits
   taustins := Readkey;
   If Ord(taustins) = 27 Then Halt
   Else Goto Starts;
